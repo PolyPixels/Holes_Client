a6b347226a34be0f973d6112686be93e44602297
David Kozdra <davidkozdra@gmail.com>
2025-03-25 18:37:24 -0400

    item counts

--- game/classes/invblock.js
+++ game/classes/invblock.js
@@ -62,59 +62,84 @@ class InvBlock{
         //spawn a bag?
     }
 
-    renderHotBar(){
-        if(this.useTimer > 0) this.useTimer--;
-        if(this.animationTimer > 0) this.animationTimer-= 0.1;
-        if(this.animationTimer < 0) this.animationTimer+= 0.1;
-
+    renderHotBar() {
+        if (this.useTimer > 0) this.useTimer--;
+        if (this.animationTimer > 0) this.animationTimer -= 0.1;
+        if (this.animationTimer < 0) this.animationTimer += 0.1;
+    
         // Hotbar Inventory
         push();
         noFill();
         stroke(255);
         strokeWeight(50);
         arc(width, height, 595, 595, 180, 270);
-
+    
         stroke("#604030");
         strokeWeight(45);
         arc(width, height, 595, 595, 180, 270);
-
+    
         imageMode(CENTER);
-        let slots = [this.selectedHotBar-2,this.selectedHotBar-1,this.selectedHotBar,this.selectedHotBar+1,this.selectedHotBar+2];
-        for(let i = 0; i < slots.length; i++){
-            if(slots[i] < 0) slots[i] = 5 + slots[i];
-            if(slots[i] > 4) slots[i] = slots[i] - 5;
+    
+        // Figure out which slots to show
+        let slots = [
+          this.selectedHotBar - 2,
+          this.selectedHotBar - 1,
+          this.selectedHotBar,
+          this.selectedHotBar + 1,
+          this.selectedHotBar + 2
+        ];
+        for (let i = 0; i < slots.length; i++) {
+          if (slots[i] < 0) slots[i] = 5 + slots[i];
+          if (slots[i] > 4) slots[i] = slots[i] - 5;
         }
-
-        // if(this.hotbar[slots[0]] != "") this.items[this.hotbar[slots[0]]].renderImage(width - (0.965 * 300), height - (0.258 * 300));
-        // if(this.hotbar[slots[1]] != "") this.items[this.hotbar[slots[1]]].renderImage(width - (0.866 * 300), height - (0.5 * 300));
-        // if(this.hotbar[slots[2]] != "") this.items[this.hotbar[slots[2]]].renderImage(width - (0.707 * 300), height - (0.707 * 300));
-        // if(this.hotbar[slots[3]] != "") this.items[this.hotbar[slots[3]]].renderImage(width - (0.5 * 300), height - (0.866 * 300));
-        // if(this.hotbar[slots[4]] != "") this.items[this.hotbar[slots[4]]].renderImage(width - (0.258 * 300), height - (0.965 * 300));
-
-        for(let i=0; i < slots.length; i++){
-            if(this.hotbar[slots[i]] != ""){
-                this.items[this.hotbar[slots[i]]].renderImage(
-                    width - (cos(15 + (15*i) + (15*this.animationTimer)) *300), 
-                    height - (sin(15 + (15*i) + (15*this.animationTimer)) *300)
-                );
+    
+        // Render the items in each slot
+        for (let i = 0; i < slots.length; i++) {
+          let slotIndex = slots[i];
+          let hotBarKey = this.hotbar[slotIndex];
+          if (hotBarKey !== "") {
+            // Compute position based on your original logic
+            let x = width  - (cos(15 + 15*i + 15*this.animationTimer) * 300);
+            let y = height - (sin(15 + 15*i + 15*this.animationTimer) * 300);
+    
+            // Draw the item image
+            let item = this.items[hotBarKey];
+            item.renderImage(x, y);
+    
+            // If there's more than one of this item, render the amount
+            if (item.amount > 1) {
+                console.log(item.amount, item)
+              push();
+              fill(255);
+              stroke(0);
+              strokeWeight(4);
+              textAlign(RIGHT, BOTTOM);
+              textSize(18);
+              textFont(gameUIFont);
+              // Adjust the offsets as needed to place the text nicely
+              text(item.amount, x + 35, y +15);
+              pop();
             }
+          }
         }
-        
+    
+        // Q & E key rectangles
         fill(150);
         stroke(255);
         strokeWeight(1);
         rectMode(CENTER);
-
-        rect(width - (0.866 * 255), height - (0.5 * 255), 20, 20); //rect for "q" key
-        rect(width - (0.5 * 255), height - (0.866 * 255), 20, 20); //rect for "e" key
-
-        fill(255,0,0);
+        rect(width - (0.866 * 255), height - (0.5 * 255), 20, 20);  // Q
+        rect(width - (0.5 * 255), height - (0.866 * 255), 20, 20);  // E
+    
+        // That triangular shape
+        fill(255, 0, 0);
         beginShape();
-        vertex(width - (0.676 * 250), height - (0.737 * 250)); //left
-        vertex(width - (0.737 * 250), height - (0.676 * 250)); //right
-        vertex(width - (0.707 * 265), height - (0.707 * 265)); //center
+        vertex(width - (0.676 * 250), height - (0.737 * 250));
+        vertex(width - (0.737 * 250), height - (0.676 * 250));
+        vertex(width - (0.707 * 265), height - (0.707 * 265));
         endShape(CLOSE);
-
+    
+        // Q/E text
         fill(0);
         stroke(0);
         textAlign(CENTER, CENTER);
@@ -122,6 +147,8 @@ class InvBlock{
         textFont(gameUIFont);
         text("Q", width - (0.866 * 255), height - (0.5 * 255) - 3);
         text("E", width - (0.5 * 255), height - (0.866 * 255) - 1.5);
+    
         pop();
     }
+    
 }
