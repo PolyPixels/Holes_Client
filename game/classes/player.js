7b98d4646fc2a1affd3753899215e58ccbb33e62
Christian Rodriguez <christiantrodriguez3@gmail.com>
2025-03-26 00:05:15 -0400

    better ground collision, allowed slight overlap when building, move obj healthbar to render

--- game/classes/player.js
+++ game/classes/player.js
@@ -33,34 +33,120 @@ class Player {
         this.animationType = ""; // Name of current animation
     }
 
-    newCollisionPoint(xOffset, yOffset) {
+    newCollisionPoint(xOffset, yOffset, direction) {
         let chunkPos = testMap.globalToChunk(this.pos.x+(xOffset*TILESIZE), this.pos.y+(yOffset*TILESIZE));
+
+        if(testMap.chunks[chunkPos.x+","+chunkPos.y] == undefined){ //if you dont have that chunk assume there is dirt in the way
+            return {
+                dir: direction,
+                val: -1
+            };
+        }
         
         let x = floor(this.pos.x / TILESIZE) - (chunkPos.x*CHUNKSIZE) + xOffset;
         let y = floor(this.pos.y / TILESIZE) - (chunkPos.y*CHUNKSIZE) + yOffset;
+
+        let x2 = floor(this.pos.x / TILESIZE) - (chunkPos.x*CHUNKSIZE) + xOffset;
+        let y2 = floor(this.pos.y / TILESIZE) - (chunkPos.y*CHUNKSIZE) + yOffset;
+
+        if(direction == "up"){
+            y2 -= 1;
+        }
+        if(direction == "down"){
+            y2 += 1;
+        }
+        if(direction == "left"){
+            x2 -= 1;
+        }
+        if(direction == "right"){
+            x2 += 1;
+        }
+
+        let chunkPos2 = {};
+        chunkPos2.x = chunkPos.x;
+        chunkPos2.y = chunkPos.y;
+        if(x2 < 0){
+            chunkPos2.x -= 1;
+            x2 = CHUNKSIZE-1;
+        }
+        if(x2 >= CHUNKSIZE){
+            chunkPos2.x += 1;
+            x2 = 0;
+        }
+        if(y2 < 0){
+            chunkPos2.y -= 1;
+            y2 = CHUNKSIZE-1;
+        }
+        if(y2 >= CHUNKSIZE){
+            chunkPos2.y += 1;
+            y2 = 0;
+        }
+
+        if(testMap.chunks[chunkPos2.x+","+chunkPos2.y] == undefined){ //if you dont have that chunk assume there is dirt in the way
+            return {
+                dir: direction,
+                val: -1
+            };
+        }
+
+        //MATH
+        let val = testMap.chunks[chunkPos.x+","+chunkPos.y].data[x + (y / CHUNKSIZE)];
+        let val2 = testMap.chunks[chunkPos2.x+","+chunkPos2.y].data[x2 + (y2 / CHUNKSIZE)];
+
+        if(val == -1 || val2 == -1){
+            return {
+                dir: direction,
+                val: -1
+            };
+        }
+
+        val+=0.7;
+        val2+=0.7;
+
+        let midpoint = {x: 0, y: 0};
+        let amt = 0;
+        if(direction == "up" || direction == "down"){
+            midpoint.x = x;
+        }
+        if(direction == "left" || direction == "right"){
+            midpoint.y = y;
+        }
+
+        if(direction == "up"){
+            amt = (1-val)/(val2-val);
+            midpoint.y = lerp(y,y2,amt);
+        }
+        if(direction == "down"){
+            amt = (1-val)/(val2-val);
+            midpoint.y = lerp(y,y2,amt);
+        }
+        if(direction == "left"){
+            amt = (1-val2)/(val-val2);
+            midpoint.x = lerp(x2,x,amt);
+        }
+        if(direction == "right"){
+            amt = (1-val2)/(val-val2);
+            midpoint.x = lerp(x2,x,amt);
+        }
+
+
         if(Debuging){
             push();
             fill(255);
             circle(((x+(chunkPos.x*CHUNKSIZE))*TILESIZE)-camera.x+(width/2),((y+(chunkPos.y*CHUNKSIZE))*TILESIZE)-camera.y+(height/2), 10);
+            circle(((x2+(chunkPos2.x*CHUNKSIZE))*TILESIZE)-camera.x+(width/2),((y2+(chunkPos2.y*CHUNKSIZE))*TILESIZE)-camera.y+(height/2), 10);
+
+            fill(255,0,0);
+            circle(((midpoint.x+(chunkPos2.x*CHUNKSIZE))*TILESIZE)-camera.x+(width/2),((midpoint.y+(chunkPos2.y*CHUNKSIZE))*TILESIZE)-camera.y+(height/2), 10);
             pop();
-        }
-        
-        if(testMap.chunks[chunkPos.x+","+chunkPos.y] == undefined){ //if you dont have that chunk assume there is no dirt in the way
-            return {
-                x: (x + 0.5) * TILESIZE,
-                y: (y + 0.5) * TILESIZE,
-                cx: chunkPos.x,
-                cy: chunkPos.y,
-                val: 0
-            };
         }
 
         return {
-            x: (x + 0.5) * TILESIZE,
-            y: (y + 0.5) * TILESIZE,
-            cx: chunkPos.x,
-            cy: chunkPos.y,
-            val: testMap.chunks[chunkPos.x+","+chunkPos.y].data[x + (y / CHUNKSIZE)]
+            val: testMap.chunks[chunkPos.x+","+chunkPos.y].data[x + (y / CHUNKSIZE)],
+            val2: testMap.chunks[chunkPos2.x+","+chunkPos2.y].data[x2 + (y2 / CHUNKSIZE)],
+            x: (midpoint.x + (chunkPos2.x * CHUNKSIZE)) * TILESIZE,
+            y: (midpoint.y + (chunkPos2.y * CHUNKSIZE)) * TILESIZE,
+            dir: direction
         };
         
     }
@@ -76,30 +162,37 @@ class Player {
         if (this.holding.w) {
             this.pos.y += -BASE_SPEED*this.statBlock.stats.runningSpeed; //*(2*deltaTime/frameRate()) removed while frameRate() is low
             this.direction = 'up';
-            collisionChecks.push(this.newCollisionPoint( 1, -1));
-            collisionChecks.push(this.newCollisionPoint( 0, -1));
-            collisionChecks.push(this.newCollisionPoint( -1, -1));
+            collisionChecks.push(this.newCollisionPoint( 0, 1, this.direction));
+            collisionChecks.push(this.newCollisionPoint( 1, 1, this.direction));
         }
         if (this.holding.a) {
             this.pos.x += -BASE_SPEED*this.statBlock.stats.runningSpeed; //*(2*deltaTime/frameRate()) removed while frameRate() is low
             this.direction = 'left';
-            collisionChecks.push(this.newCollisionPoint( -1,  1));
-            collisionChecks.push(this.newCollisionPoint( -1,  0));
-            collisionChecks.push(this.newCollisionPoint( -1, -1));
+            collisionChecks.push(this.newCollisionPoint( 0,  1, this.direction));
+            if(this.holding.w){
+                collisionChecks.push(this.newCollisionPoint( 0,  2, this.direction));
+            }
+            else{
+                collisionChecks.push(this.newCollisionPoint( 0,  0, this.direction));
+            }
         }
         if (this.holding.s) {
             this.pos.y += BASE_SPEED*this.statBlock.stats.runningSpeed; //*(2*deltaTime/frameRate()) removed while frameRate() is low
             this.direction = 'down';
-            collisionChecks.push(this.newCollisionPoint( 1, 1));
-            collisionChecks.push(this.newCollisionPoint( 0, 1));
-            collisionChecks.push(this.newCollisionPoint( -1, 1));
+            collisionChecks.push(this.newCollisionPoint( 0, 1, this.direction));
+            collisionChecks.push(this.newCollisionPoint( 1, 1, this.direction));
         }
         if (this.holding.d) {
             this.pos.x += BASE_SPEED*this.statBlock.stats.runningSpeed; //*(2*deltaTime/frameRate()) removed while frameRate() is low
             this.direction = 'right';
-            collisionChecks.push(this.newCollisionPoint(1,  1));
-            collisionChecks.push(this.newCollisionPoint(1,  0));
-            collisionChecks.push(this.newCollisionPoint(1, -1));
+
+            collisionChecks.push(this.newCollisionPoint( 1,  1, this.direction));
+            if(this.holding.w){
+                collisionChecks.push(this.newCollisionPoint( 1,  2, this.direction));
+            }
+            else{
+                collisionChecks.push(this.newCollisionPoint( 1,  0, this.direction));
+            }
         }
 
         // Handle collisions
@@ -124,9 +217,12 @@ class Player {
         for (let i = 0; i < collisionChecks.length; i++) {
             let check = collisionChecks[i];
             if (check.val == -1) this.pos = oldPos;
-            if (check.val > 0) {
-                if (this.pos.dist(createVector(check.x+(check.cx*CHUNKSIZE*TILESIZE), check.y+(check.cy*CHUNKSIZE*TILESIZE))) < 16 + (check.val * TILESIZE / 2)) {
-                    this.pos = oldPos;
+            if (check.val2 > 0) {
+                if(check.dir == "up" || check.dir == "down"){
+                    if(createVector(check.x, this.pos.y).dist(createVector(check.x, check.y)) < TILESIZE) this.pos.y = oldPos.y;
+                }
+                if(check.dir == "left" || check.dir == "right"){
+                    if(createVector(this.pos.x, check.y).dist(createVector(check.x, check.y)) < TILESIZE) this.pos.x = oldPos.x;
                 }
             }
         }
