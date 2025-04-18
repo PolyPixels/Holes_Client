c8ec837ff502229e426c15623a9aab38461a0567
Christian Rodriguez <christiantrodriguez3@gmail.com>
2025-04-18 12:21:48 -0400

    correct ui update lol

--- game/input.js
+++ game/input.js
@@ -342,8 +342,7 @@ function continousMouseInput(){ //ran once every frame, good for anything like d
                                 update_values: [curPlayer.animationType,curPlayer.animationFrame]
                             });
 
-                            updateCraftList();
-                            updatecurCraftItemDiv();
+                            renderBuildOptions();
                         }
                     }
                 }
